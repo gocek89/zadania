a1a0bc07689a8feaf6517fbf862cde37bdb31fa7
Gocek <gocek89@gmail.com>
2021-11-07 15:09:46 +0100

    adding comment in program

--- palindrom.py
+++ palindrom.py
@@ -1,4 +1,9 @@
 def palindrom(word):
+    """
+       Checking is argument is palindrome:
+       arguments:
+       word 
+    """
     if list(word) == list(word[::-1]):
         return True
     else:
