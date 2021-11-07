c961d9010e242767b0255defafe01a732aa2c078
Gocek <gocek89@gmail.com>
2021-11-07 15:17:54 +0100

    add rest of code, wher is calling the fun and if test of result

--- palindrom.py
+++ palindrom.py
@@ -1,10 +1,20 @@
-def palindrom(word):
+def palindrom(word_to_checkt):
     """
        Checking is argument is palindrome:
        arguments:
-       word 
+       word_to_checkt
     """
-    if list(word) == list(word[::-1]):
+    if list(word_to_checkt) == list(word_to_checkt[::-1]):
         return True
     else:
         return False
+
+
+print("Program sprawdza czy podane słowo jest palindromem tj. czy brzmi ono tak samo pisane od lewej do prawej jak rowniez od prawej do lewej")
+text = str(input("Proszę podać słowo do sprawdzenia:\n").lower())
+word = palindrom(text)
+
+if word == True:
+    print(f"Słowo {text} jest pailindromem")
+else:
+    print(f"Słowo {text} nie jest palindromem")
